54393a9f53c2b7aa534686c15d0ee7e403bd2709
nur-jowel <99303126+nur-jowel@users.noreply.github.com>
2022-10-27 08:35:25 +0600

    Add files via upload

---
+++ admission.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    double gpa;
+    scanf("%lf",&gpa);
+    if (gpa<5.00)
+    {
+        printf("No\n");
+    }
+    else
+        printf("Yes.");
+    return 0;
+}
